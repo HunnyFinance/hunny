b2f51cff80b9765a0a504490947c0d5a0388a79f
PancakeHunny <tech@pancakehunny.finance>
2021-06-04 16:58:47 +0700

    add vault cake to cake

---
+++ contracts/vaults/VaultController.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
+
+/*
+*
+* MIT License
+* ===========
+*
+* Copyright (c) 2020 HunnyFinance
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*/
+
+import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/SafeBEP20.sol";
+import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/BEP20.sol";
+
+import "../interfaces/IPancakeRouter02.sol";
+import "../interfaces/IPancakePair.sol";
+import "../interfaces/IStrategy.sol";
+import "../interfaces/IMasterChef.sol";
+import "../interfaces/IHunnyMinterV2.sol";
+import "../interfaces/IHunnyChef.sol";
+import "../library/PausableUpgradeable.sol";
+import "../library/WhitelistUpgradeable.sol";
+
+
+abstract contract VaultController is IVaultController, PausableUpgradeable, WhitelistUpgradeable {
+    using SafeBEP20 for IBEP20;
+
+    /* ========== CONSTANT VARIABLES ========== */
+    BEP20 private constant HUNNY = BEP20(0x565b72163f17849832A692A3c5928cc502f46D69);
+
+    /* ========== STATE VARIABLES ========== */
+
+    address public keeper;
+    IBEP20 internal _stakingToken;
+    IHunnyMinterV2 internal _minter;
+    IHunnyChef internal _hunnyChef;
+
+    /* ========== VARIABLE GAP ========== */
+
+    uint256[49] private __gap;
+
+    /* ========== Event ========== */
+
+    event Recovered(address token, uint amount);
+
+
+    /* ========== MODIFIERS ========== */
+
+    modifier onlyKeeper {
+        require(msg.sender == keeper || msg.sender == owner(), 'VaultController: caller is not the owner or keeper');
+        _;
+    }
+
+    /* ========== INITIALIZER ========== */
+
+    function __VaultController_init(IBEP20 token) internal initializer {
+        __PausableUpgradeable_init();
+        __WhitelistUpgradeable_init();
+
+        keeper = 0xe5F7E3DD9A5612EcCb228392F47b7Ddba8cE4F1a;
+        _stakingToken = token;
+    }
+
+    /* ========== VIEWS FUNCTIONS ========== */
+
+    function minter() external view override returns (address) {
+        return canMint() ? address(_minter) : address(0);
+    }
+
+    function canMint() internal view returns (bool) {
+        return address(_minter) != address(0) && _minter.isMinter(address(this));
+    }
+
+    function hunnyChef() external view override returns (address) {
+        return address(_hunnyChef);
+    }
+
+    function stakingToken() external view override returns (address) {
+        return address(_stakingToken);
+    }
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    function setKeeper(address _keeper) external onlyKeeper {
+        require(_keeper != address(0), 'VaultController: invalid keeper address');
+        keeper = _keeper;
+    }
+
+    function setMinter(address newMinter) virtual public onlyOwner {
+        // can zero
+        _minter = IHunnyMinterV2(newMinter);
+        if (newMinter != address(0)) {
+            require(newMinter == HUNNY.getOwner(), 'VaultController: not hunny minter');
+            _stakingToken.safeApprove(newMinter, 0);
+            _stakingToken.safeApprove(newMinter, uint(~0));
+        }
+    }
+
+    function setHunnyChef(IHunnyChef newHunnyChef) virtual public onlyOwner {
+        require(address(_hunnyChef) == address(0), 'VaultController: setHunnyChef only once');
+        _hunnyChef = newHunnyChef;
+    }
+
+    /* ========== SALVAGE PURPOSE ONLY ========== */
+
+    function recoverToken(address _token, uint amount) virtual external onlyOwner {
+        require(_token != address(_stakingToken), 'VaultController: cannot recover underlying token');
+        IBEP20(_token).safeTransfer(owner(), amount);
+
+        emit Recovered(_token, amount);
+    }
+}
